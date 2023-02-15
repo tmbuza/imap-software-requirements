974ac37e2b421d3c992ec2ea6d952de02072978c
tmbuza <ndelly@gmail.com>
2023-02-15 10:13:51 -0600

    Update css

--- workflow/scripts/smk_html_report.sh
+++ workflow/scripts/smk_html_report.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # Generate interactive html report
-snakemake --report report.html
-
-hti -H report.html -o images/smkreport
+snakemake --report report.html | hti -H report.html -o images/smkreport
 
